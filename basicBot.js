8a6efcc7943bfc98b0a237cf91a27dee76905f7e
Churche <xmto.canadax@gmail.com>
2015-04-20 12:38:44 -0400

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1711,8 +1711,8 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat(subChat(basicBot.chat.commandslink, {botname: basicBot.settings.botName, link: http://tinyurl.com/acheaacmds}));
-                    }
+
+                        API.sendChat(subChat(basicBot.chat.commandslink, {botname: basicBot.settings.botName, link: basicBot.cmdLink}));                    }
                 }
             },
 
