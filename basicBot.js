421a55004779300b729e06a3a9842364f2a4cf79
Churche <xmto.canadax@gmail.com>
2015-05-07 00:53:43 -0400

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -340,7 +340,7 @@
                     basicBot.room.roulette.rouletteStatus = true;
                     basicBot.room.roulette.countdown = setTimeout(function () {
                         basicBot.room.roulette.endRoulette();
-                    }, 60 * 1000);
+                    }, 90 * 1000);
                     API.sendChat(basicBot.chat.isopen);
                 },
                 endRoulette: function () {
